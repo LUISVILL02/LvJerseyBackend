4f879238b295a11f6f039a35e4be2f18c819a4ab
Luisferbillete <luisfernandovllamil@gmail.com>
2025-09-19 20:13:02 -0500

    Implement authentication endpoints for registration, login, and token refresh

--- LvJerseyApi/Controllers/AuthController.cs
+++ LvJerseyApi/Controllers/AuthController.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Authentication.Application.Commands;
+using Authentication.Application.Commands.Login;
+using Authentication.Application.Commands.RefreshToken;
+using Authentication.Application.Commands.Register;
+using Authentication.Application.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Infrastructure.Abstractions;
 
 namespace LvJerseyApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class AuthController : ControllerBase
+[Route("api/v0.0.1/[controller]")]
+public class AuthController(ISender sender) : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Get()
+    [HttpPost, Route("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterCommand register)
     {
-        return Ok("Helper");
+        await sender.SendCommandAsync<RegisterCommand, bool>(register);
+        return Created();
+    }
+
+    [HttpPost, Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginCommand login)
+    {
+        var response = await sender.SendCommandAsync<LoginCommand, AuthResponseDto>(login);
+        return Ok(response);
+    }
+
+    [HttpPost, Route("refresh")]
+    public async Task<IActionResult> Refresh([FromBody] RefreshTokenCommand refresh)
+    {
+        var response = await sender.SendCommandAsync<RefreshTokenCommand, AuthResponseDto>(refresh);
+        return Ok(response);
     }
 }
