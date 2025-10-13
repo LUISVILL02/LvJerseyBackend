6822c09b597547bc517b2ea80be97ff5d3ea3dc0
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 14:52:47 -0500

    Add ServiceCollectionExtensions for assembly-based service registration

---
+++ Shared/Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Application.Abstractions;
+using Shared.Infrastructure.Abstractions;
+
+namespace Shared.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AssemblyRegister(this IServiceCollection services, Assembly assembly)
+    {
+        var handlerTypes = assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface)
+            .Select(t => new
+            {
+                Type = t,
+                Interfaces = t.GetInterfaces()
+                    .Where(i => i.IsGenericType &&
+                                (i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>) ||
+                                 i.GetGenericTypeDefinition() == typeof(IQueryHandler<,>)))
+            });
+
+        foreach (var ht in handlerTypes)
+        {
+            foreach (var @interface in ht.Interfaces)
+            {
+                services.AddScoped(@interface, ht.Type);
+            }
+        }
+
+        return services;
+    }
+}
