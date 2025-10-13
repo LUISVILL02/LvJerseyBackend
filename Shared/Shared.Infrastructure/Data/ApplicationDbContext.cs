fab806b62522f5c3f52cdb702102eb92046aec4c
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 14:51:42 -0500

    Add ApplicationDbContext for managing database configurations and model creation

---
+++ Shared/Shared.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Importar configuraciones de cada módulo
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => a.GetName().Name!.EndsWith(".Infrastructure"))
+            .ToList();
+
+        foreach (var assembly in assemblies)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+        base.OnModelCreating(modelBuilder);
+    }
+};
+
+
