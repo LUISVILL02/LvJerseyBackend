fbc5534854921c28cb56fac81c88020905d80f48
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 14:51:19 -0500

    Add ISender interface and Sender implementation for command and query handling

---
+++ Shared/Shared.Application/Abstractions/ISender.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Application.Abstractions;
+
+public interface ISender
+{
+    Task<TResult> SendCommandAsync<TCommand, TResult>(TCommand command);
+    Task<TResult> SendQueryAsync<TQuery, TResult>(TQuery query);
+}
